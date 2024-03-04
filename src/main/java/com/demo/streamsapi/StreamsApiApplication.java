8eabb43dd7567b195fd38b4fa7cd66bc37b58a8f
shehan tharaka <shehantharaka000@gmail.com>
2024-03-04 08:34:32 +0530

    Initial Commit

---
+++ src/main/java/com/demo/streamsapi/StreamsApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.streamsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/streamsapi/StreamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.streamsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
